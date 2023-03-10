123cd1b9c1bf8fb5d9d5f4cbf28943a3bef0f040
Anton Kovunov <anton_kovunov@intuit.com>
2023-03-10 15:30:43 -0500

    lecture(security): add entity name to avoid naming collision in database

--- src/main/java/com/cpan252/tekkenreborn/model/User.java
+++ src/main/java/com/cpan252/tekkenreborn/model/User.java
@@ -11,16 +11,21 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@Entity
+@AllArgsConstructor
+@Entity(name = "users")
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String username;
